2da9462fa2ee86f9c9ef60d3a860222fac8a3114
saranyav4404 <153512867+saranyav4404@users.noreply.github.com>
2023-12-26 15:15:20 +0700

    Day_5

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { ApiComponent } from '../../api/api.component';
+import { ApiService } from '../../api.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  constructor(private api: ApiService){}
+
+  ngOnInit(){
+    this.fetchData()
+  }
+  data:any
+
+  fetchData(){
+
+    this.api.getData().subscribe((response:any) =>{
+      this.data = response;
+    }
+    )
+  }
+
+
+}
